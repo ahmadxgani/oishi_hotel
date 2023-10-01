569ac7dc14886171898eed908a64238ebdbff4cf
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-02 06:21:35 +0700

    update view

---
+++ resources/views/components/feature.blade.php
@@ -0,0 +1,47 @@
+<div class="container px-4 py-5" id="featured-3">
+    <h2 class="pb-2 border-bottom">Feature</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+        <div class="feature col">
+            <div
+                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                <i class="bi bi-currency-dollar"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">Reserve Room</h3>
+            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                and
+                probably just keep going until we run out of words.</p>
+            <a href="#" class="icon-link">
+                Call to action
+                <i class="bi bi-chevron-right"></i>
+            </a>
+        </div>
+        <div class="feature col">
+            <div
+                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                <i class="bi bi-house"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">List Room</h3>
+            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                and
+                probably just keep going until we run out of words.</p>
+            <a href="#" class="icon-link">
+                Call to action
+                <i class="bi bi-chevron-right"></i>
+            </a>
+        </div>
+        <div class="feature col">
+            <div
+                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                <i class="bi bi-toggles2"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">Hotel Facility</h3>
+            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                and
+                probably just keep going until we run out of words.</p>
+            <a href="#" class="icon-link">
+                Call to action
+                <i class="bi bi-chevron-right"></i>
+            </a>
+        </div>
+    </div>
+</div>
