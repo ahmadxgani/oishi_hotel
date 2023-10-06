fea39a539a6079dada34e97f301c39e115f78352
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-06 18:54:47 +0700

    fix: error foreign key and delete correspond photo on room type deleted
    https://t.me/c/1987506309/3/696

--- app/Models/TypeRoom.php
+++ app/Models/TypeRoom.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 
 class TypeRoom extends Model
 {
     use HasFactory;
     public $fillable = ['name', 'description', 'publish_rate'];
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(RoomPhoto::class);
+    }
+
+    protected static function booted () {
+        static::deleting(function(TypeRoom $typeRoom) {
+            if (!$typeRoom->photos->isEmpty()) {
+                foreach ($typeRoom->photos as $photo) {
+                    Storage::delete($photo->image);
+                }
+            }
+        });
+    }
 }
