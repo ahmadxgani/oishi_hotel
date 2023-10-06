0c58dfc7554354b58f8d5bdda40adfcde72559e8
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-06 17:30:19 +0700

    update page detail

--- resources/views/admin/room/detail.blade.php
+++ resources/views/admin/room/detail.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.dashboard')
+@vite(['resources/css/room.css'])
 
 @section('content')
     <div class="main-content container-fluid">
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Room Number {{ $room->no_room }}</h3>
+                    <h3>Detail Types of Rooms & Suite</h3>
                     <p class="text-subtitle text-muted">We use 'simple-datatables' made by @fiduswriter. You can check the
                         full documentation <a href="https://github.com/fiduswriter/Simple-DataTables/wiki">here</a>.</p>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <div class="float-end">
-                        <a href="{{ route('admin.room.create') }}" class="btn btn-primary">Add Room</a>
+                        <a href="{{ route('admin.room.create') }}" class="btn btn-primary">Add Room <i
+                                data-feather="plus"></i></a>
                     </div>
                 </div>
             </div>
@@ -26,33 +28,49 @@
                         <thead>
                             <tr>
                                 <th>ID</th>
+                                <th>Name</th>
+                                <th>Description</th>
                                 <th>Publish Rate</th>
-                                <th>Type Room</th>
                                 <th>Created At</th>
                                 <th>Updated At</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($rooms as $r)
-                                <tr>
-                                    <td>{{ $r->id }}</td>
-                                    <td>{{ $r->publish_rate }}</td>
-                                    <td>{{ $r->type() }}</td>
-                                    <td>{{ $r->created_at }}</td>
-                                    <td>{{ $r->updated_at }}</td>
-                                    <td>
-                                        <a href="{{ route('admin.room.edit', $r->id) }}" class="btn btn-primary">Edit</a>
-                                        <form action="{{ route('admin.room.destroy', $r->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
+                            <tr>
+                                <td>{{ $room->id }}</td>
+                                <td>{{ $room->name }}</td>
+                                <td>
+                                    <div class="hide-partially" style="width: 500px">
+                                        {{ $room->description }}
+                                    </div>
+                                </td>
+                                <td>{{ $room->publish_rate }}</td>
+                                <td>{{ $room->created_at }}</td>
+                                <td>{{ $room->updated_at }}</td>
+                                <td>
+                                    <div class="row">
+                                        <div class="col">
+                                            <a href="{{ route('admin.room.edit', $room->id) }}"
+                                                class="btn btn-sm btn-primary"><i data-feather="edit"></i></a>
+                                        </div>
+                                        <div class="col">
+                                            <a href="{{ route('admin.room.show', $room->id) }}"
+                                                class="btn btn-sm btn-primary"><i data-feather="eye"></i></a>
+                                        </div>
+                                        <div class="col">
+                                            <form action="{{ route('admin.room.destroy', $room->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
 
-                                            <button type="submit" class="btn btn-danger">Delete</button>
-                                        </form>
+                                                <button type="submit" class="btn btn-sm btn-danger"><i
+                                                        data-feather="trash"></i></button>
+                                            </form>
+                                        </div>
+                                    </div>
 
-                                    </td>
-                                </tr>
-                            @endforeach
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
