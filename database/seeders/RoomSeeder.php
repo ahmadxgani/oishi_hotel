dfcfdf93e280e7bf734560b1d9bae6ea6e55613d
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-08 15:14:29 +0700

    add room seeder

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Room;
 use Illuminate\Database\Seeder;
 
 class RoomSeeder extends Seeder
@@ -12,6 +12,13 @@ class RoomSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        for ($i=1; $i <= 500; $i++) {
+            Room::create([
+                'no_room' => $i,
+                'type_room_id' => rand(1, 3)
+            ]);
+        }
+
     }
 }
