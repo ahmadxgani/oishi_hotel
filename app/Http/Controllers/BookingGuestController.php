fbeb3e152dcdb1e14f2644c2ba33ee9578fd25a5
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-11 00:06:23 +0700

    handle on create book item failed

--- app/Http/Controllers/BookingGuestController.php
+++ app/Http/Controllers/BookingGuestController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Booking;
+use App\Models\BookingItem;
+use App\Models\RoomType;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BookingGuestController extends Controller
 {
@@ -20,7 +25,8 @@ class BookingGuestController extends Controller
      */
     public function create()
     {
-        return view('guest.reserve');
+        $rooms = RoomType::all();
+        return view('guest.reserve', compact('rooms'));
     }
 
     /**
@@ -28,13 +34,33 @@ class BookingGuestController extends Controller
      */
     public function store(Request $r)
     {
-        $book = Booking::create([
-            "room_id"       => $i,
-            "date_book_end" => $r->,
-            "date_book_start" => $,
-        ]);
-        for ($i = 1; $i <= $r->nr_rooms; $i++) {
+        try {
+            DB::beginTransaction();
 
+            $book = Booking::create([
+                "user_id"         => Auth::user()->id,
+                "date_book_end"   => $r->date_book_end,
+                "date_book_start" => $r->date_book_start,
+                "nr_adults"       => $r->nr_adults,
+                "nr_children"     => $r->nr_children,
+                "total_price"     => 55,
+            ]);
+
+            for ($i = 1; $i <= $r->nr_rooms; $i++) {
+                BookingItem::create([
+                    "booking_id" => $book->id,
+                    "room_id"    => 1, // TODO(): auto chosen by backend
+                    "price"      => 1,
+                ]);
+            }
+
+            DB::commit();
+            toast('Successfully created a new booking', 'success');
+            return redirect()->route('booking_guest.index');
+        } catch (Exception $e) {
+            DB::rollBack();
+            toast($e->getMessage(), 'error');
+            return redirect()->route('booking_guest.create');
         }
     }
 
